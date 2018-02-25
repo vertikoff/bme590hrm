265e76ff1d60e2612c6fc5627cdd88415cf1e67b
Cole Vertikoff <cole.vertikoff@gmail.com>
2018-02-25 11:16:35 -0500

    Adds set_duration method to HeartRateMonitor class.
    Additionally, test statements for the method have been added to the test_heart_rate_monitor.py module

--- test_heart_rate_monitor.py
+++ test_heart_rate_monitor.py
@@ -11,7 +11,16 @@ def test_voltage_extremes():
     import pytest
     from heart_rate_monitor import HeartRateMonitor
     a = HeartRateMonitor('test_data/test_data1.csv').voltage_extremes
-    assert a == ('-0.005', '1.05')
+    assert a == (-0.68, 1.05)
 
     with pytest.raises(ImportError):
         HeartRateMonitor('fake_dir/not_real.csv').voltage_extremes
+
+def test_duration():
+    import pytest
+    from heart_rate_monitor import HeartRateMonitor
+    a = HeartRateMonitor('test_data/test_data1.csv').duration
+    assert a == 27.775
+
+    with pytest.raises(ImportError):
+        HeartRateMonitor('fake_dir/not_real.csv').duration
