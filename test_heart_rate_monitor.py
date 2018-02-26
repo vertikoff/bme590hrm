e263183c339ff09c81d7fe4d06e65f319a7ea9a8
Cole Vertikoff <cole.vertikoff@gmail.com>
2018-02-26 11:08:16 -0500

    Updates max_voltages method to use built in python min and max list methods rather than looping over all data

--- test_heart_rate_monitor.py
+++ test_heart_rate_monitor.py
@@ -8,14 +8,13 @@ def test_import_csv():
 
 
 def test_voltage_extremes():
-    pass
-    # import pytest
-    # from heart_rate_monitor import HeartRateMonitor
-    # a = HeartRateMonitor('test_data/test_data1.csv').voltage_extremes
-    # assert a == (-0.68, 1.05)
-    #
-    # with pytest.raises(ImportError):
-    #     HeartRateMonitor('fake_dir/not_real.csv').voltage_extremes
+    import pytest
+    from heart_rate_monitor import HeartRateMonitor
+    a = HeartRateMonitor('test_data/test_data1.csv').voltage_extremes
+    assert a == (-0.68, 1.05)
+
+    with pytest.raises(ImportError):
+        HeartRateMonitor('fake_dir/not_real.csv').voltage_extremes
 
 
 def test_duration():
